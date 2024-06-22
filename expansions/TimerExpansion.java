e2eeaa779e3952f3c423b304abec8fb8156ee8f6
263486489298 <169001222+263486489298@users.noreply.github.com>
2024-06-22 13:20:54 -0400

    SSSSSSs
    SSSSSSSSSSSSSSSSSSSSS

---
+++ expansions/TimerExpansion.java
@@ -0,0 +1,22 @@
+package me.clip.placeholderapi.expansions;
+
+import org.bukkit.OfflinePlayer;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+
+public class Timer extends PlaceholderExpansion {
+
+    @Override
+    public String getAuthor() {
+        return "Driftplayzmc";
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "keyall-timer";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0.0";
+    }
+}
